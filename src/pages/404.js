b62fd07500a6dd292eb0292d65c01f2fea2067c5
rohan <rohanrajgautam@gmail.com>
2020-09-13 12:26:30 +0545

    added custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,21 @@
 import React from "react"
-
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
+import styles from "../css/error.module.css"
+import Banner from "../components/Banner"
 
 const Error = () => {
-  return <Layout>404 error page</Layout>
+  return (
+    <Layout>
+      <div className={styles.error}>
+        <Banner title="oops it's a dead end">
+          <Link to="/" className="btn-white">
+            back to home page
+          </Link>
+        </Banner>
+      </div>
+    </Layout>
+  )
 }
 
 export default Error
