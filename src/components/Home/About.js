9c7abbcd01aeb270980e045a148350c647837363
rohan <rohanrajgautam@gmail.com>
2020-09-14 16:12:01 +0545

    changed homepage aboutimage into gatsby image for optimization

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,16 +1,30 @@
 import React from "react"
 import Title from "../Title"
 import styles from "../../css/about.module.css"
-import img from "../../images/defaultBcg.jpeg"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+
+const getAbout = graphql`
+  query aboutImage {
+    aboutImage: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 600) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 const About = () => {
+  const { aboutImage } = useStaticQuery(getAbout)
   return (
     <section className={styles.about}>
       <Title title="About" subtitle="Us" />
       <div className={styles.aboutCenter}>
         <article className={styles.aboutImg}>
           <div className={styles.imgContainer}>
-            <img src={img} alt="about-company" />
+            <Img fluid={aboutImage.childImageSharp.fluid} alt="about image" />
           </div>
         </article>
         <article className={styles.aboutInfo}>
--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../example/Images"
 
 const Blog = () => {
   return (
     <Layout>
       <h4>hello from blog</h4>
-      <Images />
     </Layout>
   )
 }
