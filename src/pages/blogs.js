a3f47bc1ef007f3c71754566f13f99ef3314ab2d
rohan <rohanrajgautam@gmail.com>
2020-09-14 15:44:39 +0545

    gatsby image example , both fixed and fluid

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import Images from "../example/Images"
 
 const Blog = () => {
   return (
     <Layout>
       <h4>hello from blog</h4>
+      <Images />
     </Layout>
   )
 }
