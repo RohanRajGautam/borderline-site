3d6ab460098102b89b690e4836a2df21d1ce641c
rohan <rohanrajgautam@gmail.com>
2020-09-14 16:50:17 +0545

    setup home page optimized background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,17 @@
 import React from "react"
 import { Link } from "gatsby"
 import Banner from "../components/Banner"
-
+import StyledHero from "../components/StyledHero"
 import Layout from "../components/Layout"
-import SimpleHero from "../components/SimpleHero"
 import About from "../components/Home/About"
 import Services from "../components/Home/Services"
+import { graphql } from "gatsby"
 
-export default () => {
+export default ({ data }) => {
+  const imageData = data.defaultBcg.childImageSharp.fluid
   return (
     <Layout>
-      <SimpleHero>
+      <StyledHero home="true" img={imageData}>
         <Banner
           title="Continue exploring"
           info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae non modi cumque alias incidunt repellat, consequuntur iusto quas numquam ad!"
@@ -19,9 +20,21 @@ export default () => {
             explore tours
           </Link>
         </Banner>
-      </SimpleHero>
+      </StyledHero>
       <About />
       <Services />
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1920) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
