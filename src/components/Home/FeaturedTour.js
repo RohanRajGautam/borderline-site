d2a2821f8356838e897bf2102d1ef390cd92a23b
rohan <rohanrajgautam@gmail.com>
2020-09-18 17:36:58 +0545

    fixed featured tour error

--- src/components/Home/FeaturedTour.js
+++ src/components/Home/FeaturedTour.js
@@ -1,33 +1,10 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import Title from "../Title"
 import styles from "../../css/items.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Tour from "../Tours/Tour"
 
-const getTours = graphql`
-  query {
-    featuredTours: allContentfulTour(filter: { featured: { eq: true } }) {
-      edges {
-        node {
-          name
-          price
-          contentful_id
-          days
-          slug
-          country
-          images {
-            fluid {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
-        }
-      }
-    }
-  }
-`
-
-const FeaturedTour = () => {
+const FeaturedTour = ({ tours }) => {
   const response = useStaticQuery(getTours)
   const tours = response.featuredTours.edges
 
